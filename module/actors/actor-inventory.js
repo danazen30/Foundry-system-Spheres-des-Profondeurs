06cdd048877640bdc85132ec6634383cf8efa531
danazen30 <danazen30@users.noreply.github.com>
2026-04-23 18:19:08 +0200

    rajout du poids

--- module/actors/actor-inventory.js
+++ module/actors/actor-inventory.js
@@ -15,14 +15,56 @@ export class SdpActorInventory {
     return total;
   }
 
-  static applyEncumbrance(actor) {
+static async applyEncumbrance(actor) {
 
-    const total = this.getTotalEncumbrance(actor);
+  let total = 0;
 
-    actor.system.resources.encumbrance ??= {};
-    actor.system.resources.encumbrance.value = total;
+  for (let item of actor.items) {
 
-    return total;
+    const enc = Number(item.system.encumbrance?.value || 0);
+    if (!enc) continue;
+
+    const qty = Number(item.system.quantity?.value ?? 1);
+
+    total += enc * qty;
+
   }
 
+    const STR = actor.system.attributes.strength.value || 0;
+const TGH = actor.system.attributes.toughness.value || 0;
+
+// 👉 moyenne arrondie à l'inférieur
+const max = Math.floor((STR + TGH) / 2);
+const ratio = total / max;
+
+let encumbranceState = {
+  level: 0,
+  label: ""
+};
+
+if (ratio > 3) {
+  encumbranceState = {
+    level: 3,
+    label: "Immobile"
+  };
+}
+else if (ratio > 2) {
+  encumbranceState = {
+    level: 2,
+    label: "-20 Agility, -2 Move, +2 Exhausted (if prolonged)"
+  };
+}
+else if (ratio > 1) {
+  encumbranceState = {
+    level: 1,
+    label: "-10 Agility, -1 Move, +1 Exhausted (if prolonged)"
+  };
+}
+
+await actor.update({
+  "system.resources.encumbrance.value": total,
+  "system.resources.encumbrance.max": max,
+  "system.resources.encumbrance.state": encumbranceState
+});
+}
 }
